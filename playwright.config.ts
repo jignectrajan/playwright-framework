4af10cfdf4d635c8c2f2d65f381142dd256add9d
jignectrajan <rajan@jignect.tech>
2025-05-19 18:47:23 +0530

    screenshot added

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   use: {
+    screenshot: 'only-on-failure', // 👈 captures screenshot when test fails
+    trace: 'on-first-retry',       // optional: captures trace for failures
     headless: false  // run in headed mode
   },
   reporter: [['list'], ['allure-playwright']],
