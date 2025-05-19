98e05e1fe0610d23f8c49bca43953f4a47e21593
jignectrajan <rajan@jignect.tech>
2025-05-19 18:05:06 +0530

    new package changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,5 +4,7 @@ export default defineConfig({
   use: {
     headless: false  // run in headed mode
   },
+ testDir: './tests',
+  testMatch: '*.spec.ts',
   reporter: [['list'], ['allure-playwright']],
 });
