04d1b093a9c6a55f96af00e9d70c1997dbc75602
jignectrajan <rajan@jignect.tech>
2025-05-19 18:40:02 +0530

    new changes in allure report

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,5 @@ export default defineConfig({
   use: {
     headless: false  // run in headed mode
   },
- testDir: './tests',
-  testMatch: '*.spec.ts',
   reporter: [['list'], ['allure-playwright']],
 });
